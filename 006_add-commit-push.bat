7011b33069d05b01ec34597b932293f260c35626
wegidog <wegidog@gmail.com>
2020-08-08 22:09:21 +0800

    Modify Commit

--- 006_add-commit-push.bat
+++ 006_add-commit-push.bat
@@ -5,7 +5,10 @@ set /p var1=請輸入要更新的版本名稱：
 @ECHO ON
 git commit -m "%var1%"
 git status
-@pause
+@ECHO OFF
+pause echo 按任意鍵即將檔案上傳至GitHub
+@ECHO ON
 git push origin master
-@echo 按任意鍵結束
+@ECHO OFF
+echo 按任意鍵結束
 pause
